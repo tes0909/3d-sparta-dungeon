4c7c0f58ecde0e1b4568cf5596c34c407c4e6734
tes0909 <krosa1213@gmail.com>
2024-10-25 15:50:20 +0900

    [Add] Health UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
+    public PlayerCondition condition;
     private void Awake()
     {
         CharacterManager.Instance.player = this;
         controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
     }
 }
