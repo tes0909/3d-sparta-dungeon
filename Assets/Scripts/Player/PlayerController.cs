606c2413d892530fe2860f3a0768276d287bcf08
tes0909 <krosa1213@gmail.com>
2024-10-29 19:30:01 +0900

    [add] invenotry, item use

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TreeEditor;
@@ -19,7 +20,9 @@ public class PlayerController : MonoBehaviour
     private float camCurXRot; 
     public float lookSensitivity;
     private Vector2 mouseDelta;
+    public bool canLook = true;
 
+    public Action inventory;
     private Rigidbody rigidbody;
 
     private void Awake()
@@ -39,7 +42,10 @@ public class PlayerController : MonoBehaviour
 
     private void LateUpdate()
     {
-        Look();
+        if(canLook)
+        {
+            Look();
+        }
     }
 
     private void Move()
@@ -82,6 +88,22 @@ public class PlayerController : MonoBehaviour
         {
             rigidbody.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
         }
+    }
+
+    public void OnInventory(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started)
+        {
+            inventory?.Invoke();
+            ToggleCursor();
+        }
+    }
+
+    void ToggleCursor()
+    {
+        bool toggle = Cursor.lockState == CursorLockMode.Locked;
+        Cursor.lockState = toggle? CursorLockMode.None : CursorLockMode.Locked;
+        canLook = !toggle;
     }
     bool IsGrounded()
     {
